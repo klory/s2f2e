589444120e8256a3d4f570448864d6797cfef88d
klory <hfdtsinghua@gmail.com>
2017-11-13 21:09:56 -0500

    prepare to 128

--- crop_and_resize.py
+++ crop_and_resize.py
@@ -7,17 +7,17 @@ sketch_filenames.sort()
 neutral_filenames = glob.glob("data/AR_scream/*.bmp")
 neutral_filenames.sort()
 
-target_size = 64
+target_size = 128
 neutral_width = 120
 neutral_height = 165
 
-# for i, name in enumerate(sketch_filenames):
-    # sketch = Image.open(name).convert('RGB')
-    # sketch = np.asarray(sketch, dtype=np.uint8)
-    # sketch = sketch[50:50+neutral_height, 40:40+neutral_width]
-    # sketch = Image.fromarray(sketch)
-    # sketch = sketch.resize((target_size, target_size))
-    # sketch.save("data/ar_s/ar_s"+str(i)+".jpg", 'JPEG')
+for i, name in enumerate(sketch_filenames):
+    sketch = Image.open(name).convert('RGB')
+    sketch = np.asarray(sketch, dtype=np.uint8)
+    sketch = sketch[50:50+neutral_height, 40:40+neutral_width]
+    sketch = Image.fromarray(sketch)
+    sketch = sketch.resize((target_size, target_size))
+    sketch.save("data/ar_s/ar_s"+str(i)+".jpg", 'JPEG')
 
 for i, name in enumerate(neutral_filenames):
     neutral = Image.open(name).convert('RGB')
