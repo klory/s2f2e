c76663a7ceaf7b80c0ff0bdd41ae66702a196593
klory <hfdtsinghua@gmail.com>
2017-11-17 22:11:00 -0500

    update download scripts to new link

--- download_data.sh
+++ download_data.sh
@@ -5,7 +5,7 @@ if [ -d "$DATA_DIR" ]; then
   echo "data already downloaded"
 else
 	mkdir "./data"
-	wget -O ./data/128.tar.gz https://drive.google.com/uc\?export\=download\&id\=1im4RFbVL5t0CdK08fu5XmJsyPvSoL1gx
+	wget -O ./data/128.tar.gz https://drive.google.com/uc\?export\=download\&id\=1gvHvc14EOItf2HeKaYTjgARE_unnGIh3
 	tar -zxvf ./data/128.tar.gz -C ./data/
 fi
 
